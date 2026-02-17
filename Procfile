6dab22b118eb63048cea4253c0fd6c24cb33a396
Aimee Little <aimeelittle1708@gmail.com>
2026-02-17 13:01:14 +0000

    Readies to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn theteachinggarden.wsgi
--- theteachinggarden/theteachinggarden/settings.py
+++ theteachinggarden/theteachinggarden/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6%0pv!e)8=koul%j&lhf06oei6o)#zpr-hqevgp5#+-t#gif%h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com','127.0.0.1', 'localhost']
 
 
 # Application definition
