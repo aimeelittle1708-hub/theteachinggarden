f20f6526dee81e7d59bce98aefc8980b8d2bd7d0
Aimee Little <aimeelittle1708@gmail.com>
2026-02-23 16:40:24 +0000

    Added Post and Comment models for admin management and moderation

--- theteachinggarden/settings.py
+++ theteachinggarden/settings.py
@@ -18,7 +18,7 @@ if ENV_PATH.exists():
         exec(code, {"os": os})
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-dev-secret-key")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
